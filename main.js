ecda7d6e602092a324578d17518741438eadf554
koliangming <liangmingko@gmail.com>
2020-10-07 14:11:14 +0800

    modified main.js

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ let user_router = require('./routers/main_router.js')
 app.get("/", (req, res) => {
     // res.send("This is main page.");
     res.send(
-        '<h1> About the Sky </h1>' +
+        '<h1> About the Sky(網址後接/stars看星空) </h1>' +
         '\n <img src="https://www.natgeomedia.com/userfiles/sm/sm804_images_A1/9136/2019081242812053.jpg" height="60%">'
     );
 })
 
-app.use("/user", user_router)
+app.use("/stars", user_router)
 
 app.listen(port, () => {
     console.log(`server listen on port ${port}`)
