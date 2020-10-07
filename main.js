224a1624f969d45cf4b0e2948156d4e4cf725eae
koliangming <liangmingko@gmail.com>
2020-10-07 19:03:06 +0800

    202010071902

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ let user_router = require('./routers/main_router.js')
 app.get("/", (req, res) => {
     // res.send("This is main page.");
     res.send(
-        '<h1> Stars in the Sky </h1>' +
-        '<h5> (網址後接/stars或點圖片看星空) </h5>' +
+        '<h1> Stars in the sky </h1>' +
+        '<h3> (網址後接/stars或點圖片看星空) </h3>' +
         '\n <a href="https://nodejs-app-final.herokuapp.com/stars"><img src="https://www.natgeomedia.com/userfiles/sm/sm804_images_A1/9136/2019081242812053.jpg" height="60%"></a>'
     );
 })
--- routers/Galaxy.js
+++ routers/Galaxy.js
@@ -8,12 +8,12 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 // define the home page route
-
 router.get('/', function (req, res) {
   res.send(
-    'Galaxy\'s home page' + '<br>' +
-    '<a href="/stars/Galaxy/about"> about Galaxy </a>' + '<br>' +
-    '<a href="/stars/Galaxy/classA.html"> Stars Discovery Online </a>'
+    '<h1> Galaxy(銀河系) </h1>' +
+    '<h2><a href="/stars/Galaxy/about"> Detail(關於銀河系的詳細介紹) </a></h2>' +
+    '<h2><a href="/stars/Galaxy/classA.html"> Stars Discovery Online(在銀河系探索行星) </a></h2>' +
+    '<img src="https://www.natgeomedia.com/userfiles/sm/sm804_images_A1/9136/2019081242812053.jpg" height="60%">'
   );
 });
 
@@ -21,7 +21,8 @@ router.get('/', function (req, res) {
 router.get('/about', function (req, res) {
   res.send(
     '<h1> Stars in the Galaxy </h1>' +
-    '\n <img src="https://www.natgeomedia.com/userfiles/sm/sm804_images_A1/9136/2019081242812053.jpg" height="60%">'
+    '<div style="width: 60%">銀河星系（古稱銀河、天河、星河、天漢、銀漢等），是一個包含太陽系的棒旋星系。直徑介於100,000光年至180,000光年。大約擁有1,000億至4,000億顆恆星，並可能有1,000億顆行星。太陽系距離銀河中心約26,000光年，在有著濃密氣體和塵埃，被稱為獵戶臂的螺旋臂的內側邊緣。在太陽的位置，公轉週期大約是2億4,000萬年。從地球看，因為是從盤狀結構的內部向外觀看，因此銀河系呈現在天球上環繞一圈的帶狀。</div>' +
+    '\n <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/ESO_-_Milky_Way.jpg/1238px-ESO_-_Milky_Way.jpg" height="60%">'
   );
 });
 
