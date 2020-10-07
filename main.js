c5fd386ff357cf85ac79eec72f7b5754401e7afb
koliangming <liangmingko@gmail.com>
2020-10-07 14:39:07 +0800

    main modified

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ let user_router = require('./routers/main_router.js')
 app.get("/", (req, res) => {
     // res.send("This is main page.");
     res.send(
-        '<h1> About the Sky(網址後接/stars看星空) </h1>' +
-        '\n <img src="https://www.natgeomedia.com/userfiles/sm/sm804_images_A1/9136/2019081242812053.jpg" height="60%">'
+        '<h1> Stars in the Sky </h1>' +
+        '<h5> (網址後接/stars或點圖片看星空) </h5>' +
+        '\n <a href="https://nodejs-app-final.herokuapp.com/stars"><img src="https://www.natgeomedia.com/userfiles/sm/sm804_images_A1/9136/2019081242812053.jpg" height="60%"></a>'
     );
 })
 
