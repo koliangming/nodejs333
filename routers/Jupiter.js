d62bcc1880c7e668ace7c2963c9a69356a1bd2cb
koliangming <liangmingko@gmail.com>
2020-10-07 16:04:21 +0800

    modified output

--- routers/Jupiter.js
+++ routers/Jupiter.js
@@ -7,10 +7,10 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 // define the home page route
-
 router.get('/', function (req, res) {
-  res.send('Jupiter\'s home page' + '<br>' +
-    '<a href="/stars/Jupiter/about"> about Jupiter</a>'
+  res.send('<h1> Jupiter(木星) </h1>' +
+    '<h2><a href="/stars/Jupiter/about">Detail(關於木星的詳細介紹)</a><h2>' +
+    '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg/450px-Jupiter_and_its_shrunken_Great_Red_Spot.jpg" height="60%">'
   );
 });
 
@@ -18,7 +18,8 @@ router.get('/', function (req, res) {
 router.get('/about', function (req, res) {
   res.send(
     '<h1> Jupiter(木星) </h1>' +
-    '\n <img src="https://www.natgeomedia.com/userfiles/sm/sm804_images_A1/9136/2019081242812053.jpg" height="60%">'
+    '<div style="width: 60%">木星是距離太陽第五近的行星，也是太陽系中體積最大的行星，目前已知有79顆衛星。木星是顆巨行星，質量是太陽的千分之一，但卻是太陽系其他行星質量總和的2.5倍。木星的主要成分是氫，但只佔十分之一分子數量的氦，卻佔了總質量的四分之一；它可能有岩石的核心和重元素[16]，但是像其他的巨行星一樣，木星沒有可以明確界定的固體表面。由於快速地自轉，木星的外觀呈現扁球體（赤道附近有輕微但明顯可見的凸起）。外面的大氣層依緯度成不同的區與帶，在彼此的交界處有湍流和風暴作用著。最顯著的例子就是大紅斑，已知是在17世紀第一次被望遠鏡見到，持續至今未曾停歇過的巨大風暴。環繞著木星的還有微弱的行星環和強大的磁層，包括4顆1610年發現的伽利略衛星，至2019年12月已經發現79顆衛星。木衛三是其中最大的一顆，其直徑大於行星中的水星。</div>' +
+    '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/NASA14135-Jupiter-GreatRedSpot-Shrinks-20140515.jpg/450px-NASA14135-Jupiter-GreatRedSpot-Shrinks-20140515.jpg" width="40%">'
   );
 });
 
--- routers/Saturn.js
+++ routers/Saturn.js
@@ -9,18 +9,18 @@ router.use(function timeLog(req, res, next) {
 // define the home page route
 
 router.get('/', function (req, res) {
-  res.send('<h1> Saturn(土星) </h1>' + '<br>' +
-    '<h2><a href="/stars/Saturn/about"> about Saturn</a><h2>' + '<br>' +
-    '\n <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Saturn_during_Equinox.jpg/450px-Saturn_during_Equinox.jpg" height="60%">'
+  res.send('<h1> Saturn(土星) </h1>' +
+    '<h2><a href="/stars/Saturn/about"> Detail(關於土星的詳細介紹)</a><h2>' +
+    '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Saturn_during_Equinox.jpg/450px-Saturn_during_Equinox.jpg" height="60%">'
   );
 });
 
 // define the about route
 router.get('/about', function (req, res) {
   res.send(
-    '<h1> Saturn(土星) </h1>' + '<br>' +
-    '<div id="cyber" style="width: 60%">土星，為太陽系八大行星之一，至太陽距離（由近到遠）位於第六、體積則僅次於木星。並與木星同屬氣體（類木）巨星。土星主要由氫組成，還有少量的氦與微痕元素[11]，內部的核心包括岩石和冰，外圍由數層金屬氫和氣體包覆著。最外層的大氣層在外觀上通常情況下都是平淡的，雖然有時會有長時間存在的特徵出現。土星的風速高達1,800公里/時，風速明顯比木星快。土星的行星磁場強度介於地球和更強的木星之間。土星有一個顯著的環系統，主要的成分是冰的微粒和較少數的岩石殘骸以及塵土。已經確認的土星的衛星有82顆，是八大行星中最多。</div>' +
-    '\n <img src="https://images.radio.com/wwjaam/Saturn.jpg?width=775&height=425&crop=4961,2728,x0,y283" height="60%">'
+    '<h1> Saturn(土星) </h1>' +
+    '<div style="width: 60%">土星，為太陽系八大行星之一，至太陽距離（由近到遠）位於第六、體積則僅次於木星。並與木星同屬氣體（類木）巨星。土星主要由氫組成，還有少量的氦與微痕元素[11]，內部的核心包括岩石和冰，外圍由數層金屬氫和氣體包覆著。最外層的大氣層在外觀上通常情況下都是平淡的，雖然有時會有長時間存在的特徵出現。土星的風速高達1,800公里/時，風速明顯比木星快。土星的行星磁場強度介於地球和更強的木星之間。土星有一個顯著的環系統，主要的成分是冰的微粒和較少數的岩石殘骸以及塵土。已經確認的土星的衛星有82顆，是八大行星中最多。</div>' +
+    '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/PIA17218_%E2%80%93_A_Farewell_to_Saturn%2C_Brightened_Version.jpg/450px-PIA17218_%E2%80%93_A_Farewell_to_Saturn%2C_Brightened_Version.jpg" width="60%">'
   );
 });
 
